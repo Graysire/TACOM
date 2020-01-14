e32d53248c96e2bf6fa777c922dbe686db5b0ea3
Graysire <graysire@dyrix.net>
2020-01-14 11:14:32 -0800

    tested grid and allowed changing between ylevel

--- TACOM/Assets/Scripts/Grid.cs
+++ TACOM/Assets/Scripts/Grid.cs
@@ -11,6 +11,24 @@ public class Grid : MonoBehaviour
     [SerializeField]
     private int numGizmos = 4; //number of gizmos to draw in each direction
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            yLayer += size;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            yLayer -= size;
+        }
+        if (yLayer % size != 0)
+        {
+            yLayer = size * Mathf.RoundToInt(yLayer / size);
+        }
+    }
+
+
+
     public Vector3 GetNearestPointOnGrid(Vector3 position)
     {
         position -= transform.position;
@@ -21,18 +39,27 @@ public class Grid : MonoBehaviour
         return new Vector3((float)xLocation * size, yLayer, (float)zLocation * size) + transform.position;
     }
 
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        for (int x = 0; x < numGizmos; x++)
+        for (int y = 0; y < numGizmos; y++)
         {
+            if (y * size == yLayer)
+            {
+                Gizmos.color = Color.red;
+            }
             for (int z = 0; z < numGizmos; z++)
             {
-                for (int y = 0; y < numGizmos; y++)
+                for (int x = 0; x < numGizmos; x++)
                 {
-                    Vector3 point = GetNearestPointOnGrid(new Vector3(x * size, 0f, z * size)) + new Vector3(0f,y * size, 0f);
+                    Vector3 point = new Vector3(x * size, y * size, z * size);
                     Gizmos.DrawSphere(point, 0.1f);
                 }
+            }
+            if (y * size == yLayer)
+            {
+                Gizmos.color = Color.yellow;
             }
         }
     }
---
+++ TACOM/Assets/Scripts/GridTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTester : MonoBehaviour
+{
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+                Debug.Log("test");
+            }
+        }
+    }
+
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        Vector3 finalPos = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPos;
+    }
+}
